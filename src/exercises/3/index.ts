7eaa0d6adc0629d281259bcb7698114d1c2ba99f
Craig Risi <39186238+CraigRisiAG@users.noreply.github.com>
2023-06-27 16:38:45 +0200

    Update index.ts

--- src/exercises/3/index.ts
+++ src/exercises/3/index.ts
@@ -1,18 +1,12 @@
 /*
 
-Intro:
-
-    Since we already have some of the additional
-    information about our users, it's a good idea
-    to output it in a nice way.
-
 Exercise:
 
-    Fix type errors in logPerson function.
+    Fix type errors in the logPerson function.
 
     logPerson function should accept both User and Admin
     and should output relevant information according to
-    the input: occupation for User and role for Admin.
+    the input: the occupation for User and role for Admin.
 
 */
 
