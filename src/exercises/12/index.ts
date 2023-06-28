f54dec01af008a02b9f8b5086da5f24d9a93641f
Craig Risi <39186238+CraigRisiAG@users.noreply.github.com>
2023-06-28 13:38:01 +0200

    Update index.ts

--- src/exercises/12/index.ts
+++ src/exercises/12/index.ts
@@ -10,28 +10,6 @@ import {
 
 /*
 
-Intro:
-
-    We have so many users and admins in the database!
-    CEO's father Jeff says that we are a BigData
-    startup now. We have no idea what it means, but
-    Jeff says that we need to do some statistics and
-    analytics.
-
-    We've ran a questionnaire within the team to
-    figure out what do we know about statistics.
-    The only person who filled it was our coffee
-    machine maintainer. The answers were:
-
-     * Maximums
-     * Minumums
-     * Medians
-     * Averages
-
-    We found a piece of code on stackoverflow and
-    compiled it into a module `stats`. The bad
-    thing is that it lacks type declarations.
-
 Exercise:
 
     Check stats module implementation at:
@@ -62,19 +40,17 @@ interface Admin {
 }
 
 const admins: Admin[] = [
-    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
-    { type: 'admin', name: 'Steve', age: 40, role: 'Steve' },
-    { type: 'admin', name: 'Will Bruces', age: 30, role: 'Overseer' },
-    { type: 'admin', name: 'Superwoman', age: 28, role: 'Customer support' }
+    { type: 'admin', name: 'Bruce Wayne', age: 38, role: 'Batman' },
+    { type: 'admin', name: 'Diana Prince', age: 27, role: 'Wonder Woman'}, 
+    { type: 'admin', name: 'Nick Fury', age: 64, role: 'The Big Boss' },
+    { type: 'admin', name: 'Bruce Banner', age: 42, role: 'The Hulk' }
 ];
 
 const users: User[] = [
-    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
-    { type: 'user', name: 'Moses', age: 70, occupation: 'Desert guide' },
-    { type: 'user', name: 'Superman', age: 28, occupation: 'Ordinary person' },
-    { type: 'user', name: 'Inspector Gadget', age: 31, occupation: 'Undercover' }
+    { type: 'user', name: 'Tony Stark', age: 45, occupation: 'Iron Man' },
+    { type: 'user', name: 'Natasha Romanoff', age: 35, occupation: 'Black Widow' }, 
+    { type: 'user', name: 'T'Challa', age: 34, role: 'Black Panther' },
+    { type: 'user', name: 'Barry Allen', age: 22, role: 'The Flash' }
 ];
 
 function logUser(user: User | null) {
